dead647ca41484c180cbbb041174367fdadf0e2c
RatyiMatyi <barta.bulcsu@diak.szbi-pg.hu>
2024-11-25 08:56:42 +0100

    f
    f

--- snail_mail.py
+++ snail_mail.py
@@ -1,4 +1,4 @@
-email = input("Your email address: ")
+
 
 # "hello.worldcom"    => An email address has to contain a '@' character!
 # "he@llo@world.com"  => An email address cannot contain more than one '@' characters!
@@ -13,14 +13,7 @@ email = input("Your email address: ")
 # "hello@world.com"   => Valid email address :)
 
 
-length_of_email = len(email)
-number_of_at_characters = email.count("@")
-number_of_dot_characters = email.count(".")
-position_of_at = email.find("@")
 
-position_of_first_dot = email.find(".")
-position_of_last_dot = email.rfind(".")
-position_of_first_dot_after_the_at = email.find(".", position_of_at)
 
 
 error_message_no_at = "An email address has to contain a '@' character!"
@@ -35,4 +28,53 @@ error_message_no_domain = "The domain after the '@' character cannot be empty!"
 error_message_invalid_username = "The username cannot start with a '.' character!"
 
 ok_message = "Valid email address :)"
-is_valid = True
+is_valid = False
+
+while not is_valid:
+    email = input("Your email address: ")
+    length_of_email = len(email)
+    number_of_at_characters = email.count("@")
+    number_of_dot_characters = email.count(".")
+    position_of_at = email.find("@")
+
+    position_of_first_dot = email.find(".")
+    position_of_last_dot = email.rfind(".")
+    position_of_first_dot_after_the_at = email.find(".", position_of_at)
+
+    if "@" not in email:
+        print(error_message_no_at)
+
+    elif email.count("@") > 1:
+        print(error_message_too_many_at)
+
+    elif number_of_dot_characters < 1:
+        print(error_message_no_dot)
+
+    elif email.startswith("@"):
+        print(error_message_no_username)
+
+    elif position_of_at == len(email) -1:
+        print(error_message_no_domain)
+
+    elif position_of_first_dot_after_the_at < 1:
+        print(error_message_no_dot_in_domain)
+
+    elif position_of_first_dot_after_the_at == position_of_at + 1:
+        print(error_message_no_server_name)
+
+    elif email.startswith("."):
+        print(error_message_invalid_username)
+
+    elif email.endswith("."):
+        print(error_message_no_tld)
+
+    elif len(email.split(".")[-1]) < 2:
+        print(error_message_short_tld)
+
+
+
+
+
+    else:
+        print(ok_message)
+        is_valid = True
